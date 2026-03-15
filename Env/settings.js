e4a2a0617d0f9b4acc9135e3de4b53554bd6fcfc
narjabdul <135540895+narjabdul@users.noreply.github.com>
2026-03-15 10:56:44 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,5 +1,5 @@
  const session = process.env.SESSION || '';
-const mycode = process.env.CODE || "255";
+const mycode = process.env.CODE || "255694128543";
 const botname = process.env.BOTNAME || 'ANDBAD-MD';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
